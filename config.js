eaf6e62f428cc03590386c9835708bb3ed4c4d0d
Radekter <radekter@gmail.com>
2026-09-22 14:56:14 +0200

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// Konfigurace připojení k vaší databázi Supabase
-const SUPABASE_URL = "https://supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5jY2VsdGZvbW9iaWdwZHNocmFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYwNzcyNzMsImV4cCI6MjAzMTY1MzI3M30.7M-gT-M6fF99oXw7x_NpxgZ7h8g3Q9V2_q8w8w-8Y_o";
