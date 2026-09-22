47e11a57ca9b727235b1ff55252412988f2c587f
Radekter <radekter@gmail.com>
2026-09-22 14:08:57 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// Konfigurace připojení k vaší databázi Supabase
+const SUPABASE_URL = "https://supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_am9zaHVhOm15c2VjcmV0a2V5MTIzNDU2Nzg5MDEyMzQ1Njc4OTA";
