958f6b33b40d0d96cbb45d20472d69233963eb5c
Radekter <radekter@gmail.com>
2026-09-22 14:40:34 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Konfigurace připojení k vaší databázi Supabase
 const SUPABASE_URL = "https://supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5jY2VsdGZvbW9iaWdwZHNocmFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYwNzcyNzMsImV4cCI6MjAzMTY1MzI3M30.7M-gT-M6fF99oXw7x_NpxgZ7h8g3Q9V2_q8w8w-8Y_o";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5jY2VsdGZvbW9iaWdwZHNocmFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAwMDc5NzEsImV4cCI6MjEwNTU4Mzk3MX0.MWa8_pFLcELq-S3B0UIKGrXmI-tHIep8CO-GtMEB9t4";
